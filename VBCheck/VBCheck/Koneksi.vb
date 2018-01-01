b6af9528594f7bb6802d49379db26bbdcdc76ffa
Apiphar <apipsisfo@gmail.com>
2018-01-01 17:20:01 +0700

    First

---
+++ VBCheck/VBCheck/Koneksi.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data.Sql
+Imports System.Data.SqlClient
+Module Koneksi
+    Public koneksi As SqlConnection
+    Public data As DataSet
+    Public baca As SqlDataReader
+    Public adaptor As SqlDataAdapter
+    Public cmd As SqlCommand
+    Public ass As DataTable
+    Public str, sql As String
+
+    Public Sub buka()
+        str = "Data Source=APIP2-PC\SQLEXPRESS;Initial Catalog=dbchecking;Persist Security Info=True;UID=sa;PWD=sasa"
+        koneksi = New SqlConnection(str)
+        Try
+            If koneksi.State = ConnectionState.Closed Then
+                koneksi.Open()
+            End If
+        Catch ex As Exception
+            MsgBox(Err.Description, MsgBoxStyle.Critical, "Error")
+        End Try
+    End Sub
+End Module
